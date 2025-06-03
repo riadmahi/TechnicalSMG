663da5bc9038d7a6f258fda9017fa290f69805db
Riad Mahi <riad.mahi@remedee.com>
2025-06-03 19:46:51 +0200

    Init iOS Project

---
+++ TechnicalSMG/TechnicalSMGApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnicalSMGApp.swift
+//  TechnicalSMG
+//
+//  Created by Riad on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TechnicalSMGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
