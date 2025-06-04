3383354ce010417ae6e1120558ff50f4ce611d58
Riad Mahi <riad.mahi@remedee.com>
2025-06-04 14:17:31 +0200

    Add basic new post view

---
+++ TechnicalSMG/Views/New Post/NewPostView.swift
@@ -0,0 +1,15 @@
+//
+//  NewPostView.swift
+//  TechnicalSMG
+//
+//  Created by Riad on 04/06/2025.
+//
+
+import SwiftUI
+
+struct NewPostView: View {
+    
+    var body: some View {
+        Text("NewPostView")
+    }
+}
