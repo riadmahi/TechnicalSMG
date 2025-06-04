0b4eb1d526c293f942991ca72ebfe889284ead06
Riad Mahi <riad.mahi@remedee.com>
2025-06-04 12:02:24 +0200

    Add post details view model

---
+++ TechnicalSMG/ViewModels/PostDetailsViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  PostDetailsViewModel.swift
+//  TechnicalSMG
+//
+//  Created by Riad on 04/06/2025.
+//
+
+
+import Foundation
+
+@MainActor
+class PostDetailsViewModel: ObservableObject {
+    
+    let post: Post
+    
+    @Published var comments: [Comment] = []
+    
+    @Published var errorMessage: String? = nil
+        
+    private let repository: APIRepository
+        
+    init(post: Post, repository: APIRepository = APIRepositoryImpl()) {
+        self.post = post
+        self.repository = repository
+    }
+    
+    func loadComments() {
+        repository.fetchComments(for: post.id) { [weak self] fetchedComments, error in
+            guard let self = self else { return }
+            
+            DispatchQueue.main.async {
+                if let error = error {
+                    self.errorMessage = error.localizedDescription
+                } else if let fetched = fetchedComments {
+                    // Keep only the first three comments
+                    self.comments = Array(fetched.prefix(3))
+                }
+            }
+        }
+    }
+}
